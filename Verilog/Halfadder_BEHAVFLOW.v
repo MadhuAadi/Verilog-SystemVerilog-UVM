05002a58625cd1cd5c7aa5c32a07739addceb252
MadhuAadi <167657673+MadhuAadi@users.noreply.github.com>
2024-12-04 22:54:47 -0800

    Update Halfadder_BEHAVFLOW.v

--- Verilog/Halfadder_BEHAVFLOW.v
+++ Verilog/Halfadder_BEHAVFLOW.v
@@ -1,1 +1,13 @@
 
+//DESIGN 
+module ha(a,b,s,c);
+  
+  input  a,b;
+  output  reg s,c;
+  
+  always@(*) begin // ALWAYS - COMBINATIONAL CIRCUIT - NO NEED OF CLKs or RESET ; SENSITIVITY LIST WILL ONLY BE THE SIGNALS, * IN SENSITIVITY LIST INCLUDES ALL SIGNALS 
+   s = a ^ b;
+   c = a*b;
+  end
+  
+endmodule  
